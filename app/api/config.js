d3ef799e09b6dc7d1578f0fde355e3135bb5c7dd
ExoDus234 <ivanpopkov06022007@gmail.com>
2024-05-26 18:47:19 +0700

    kira

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://spectrebackend.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://spectrebackend.nomoredomainswork.ru/api';
 
 export const endpoints = {
     games: `${BASE_URL}/games`,
